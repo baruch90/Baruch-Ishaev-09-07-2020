393835620f0359baf40046fee917444326e6176f
Baruch <baruch14390@gmail.com>
2020-07-19 16:36:34 +0300

    Updates

---
+++ precache-manifest.e31e281df07f03ab603a7ddb5d46579d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d630eac7588430327c80f4712457c0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c103d65e879cdef68198",
+    "url": "/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "afbdd62f76e818e78d4d",
+    "url": "/my-app/static/css/main.b799867e.chunk.css"
+  },
+  {
+    "revision": "c103d65e879cdef68198",
+    "url": "/my-app/static/js/2.26791472.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-app/static/js/2.26791472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbdd62f76e818e78d4d",
+    "url": "/my-app/static/js/main.5ee13850.chunk.js"
+  },
+  {
+    "revision": "2a73a732bdb696882d27",
+    "url": "/my-app/static/js/runtime-main.6b58c57d.js"
+  }
+]);
